ce2c851bc20570b62c68ce8e4b0d7c94db7bbf7d
Kesha Hall <k_renee10@yahoo.com>
2020-10-08 20:18:46 -0400

    added initial files and import/export

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("The Artery Clogger", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("The Tantilizer", true);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("The Cheat Day", false);
+
