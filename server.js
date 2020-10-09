199668853ecef1e5e0ee6c46e86f509222ab8fbb
Kesha Hall <k_renee10@yahoo.com>
2020-10-08 21:57:19 -0400

    added jawsdb

--- server.js
+++ server.js
@@ -1,33 +1,41 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
 const mysql = require("mysql");
+const connection = require("./config/connection");
 
+var app = express();
 
 var PORT = process.env.PORT || 8080;
 
-var app = express();
+
+
+
+
+// Serve static content for the app from the "public" directory in the application directory.
+// app.use(express.static("public"));
+
+// // Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// // Import routes and give the server access to them.
+// var routes = require("./controllers/catsController.js");
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
 
 app.get("/api/config", (req, res) => {
     res.json({
         success: true
     });
 });
-
-// Serve static content for the app from the "public" directory in the application directory.
-// app.use(express.static("public"));
-
-// // Parse application body as JSON
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// // Set Handlebars.
-// var exphbs = require("express-handlebars");
-
-// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-// app.set("view engine", "handlebars");
-
-// // Import routes and give the server access to them.
-// var routes = require("./controllers/catsController.js");
 
 // app.use(routes);
 
