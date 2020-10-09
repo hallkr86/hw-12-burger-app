b0d0c7f57f375d7beaaf1b986d5c089b64d6681e
Kesha Hall <k_renee10@yahoo.com>
2020-10-08 20:42:43 -0400

    added config to test route

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ var PORT = process.env.PORT || 8080;
 
 var app = express();
 
+app.get("/api/config", (req, res) => {
+    res.json({
+        success: true
+    });
+});
+
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
