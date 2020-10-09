c4b24c96f0e5cf35018fbdb039bec7f68c006f94
Kesha Hall <k_renee10@yahoo.com>
2020-10-08 20:49:06 -0400

    greyed out middle ware to test app

--- server.js
+++ server.js
@@ -14,22 +14,22 @@ app.get("/api/config", (req, res) => {
 });
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
-// Parse application body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// // Parse application body as JSON
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
+// // Set Handlebars.
+// var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/catsController.js");
+// // Import routes and give the server access to them.
+// var routes = require("./controllers/catsController.js");
 
-app.use(routes);
+// app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
