2935ee9166ce3818359a646f8afbd5b60257a28d
Kesha Hall <k_renee10@yahoo.com>
2020-10-09 20:50:48 -0400

    made correction in routes

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var PORT = process.env.PORT || 8080;
 
 
 // Serve static content for the app from the "public" directory in the application directory.
-// app.use(express.static("public"));
+app.use(express.static("public"));
 
 // // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
@@ -24,7 +24,9 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // // Import routes and give the server access to them.
-// var routes = require("./controllers/catsController.js");
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
 
 app.get("/", (req, res) => {
     connection.query("SELECT * FROM burger", (err, data) => {
